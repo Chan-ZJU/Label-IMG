d98e7a836c490bbe95efc7c38fb8ad750324bbcb
Chan-ZJU <1072122585@qq.com>
2021-12-03 16:21:10 +0800

    init: create vue project and springboot project

---
+++ label-img-springboot/src/main/java/com/chan/labelimg/LabelImgSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.chan.labelimg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabelImgSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LabelImgSpringbootApplication.class, args);
+    }
+
+}
