a2bc6323f2f35389c33e1b340f3e2061c05daabb
Chan-ZJU <1072122585@qq.com>
2021-12-06 22:13:32 +0800

    vue: test video play function with oss file href

--- label-img-vue/src/components/Video.vue
+++ label-img-vue/src/components/Video.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-      <video src="../assets/video.mp4" controls style="width:300px;"></video>
+      <video src="http://chanzjuoss.oss-cn-hangzhou.aliyuncs.com/2021/12/06/28ab3c55-1640-4075-9236-6df473554e00.mp4" controls style="width:300px;"></video>
       <img :src="imgSrc">
       <div>
         <button @click="cutPicture">
